2a538b9a912a9ba764464409528f1ddb3eaec4ef
MallikarjunaM7 <158544491+MallikarjunaM7@users.noreply.github.com>
2024-11-13 10:05:14 +0530

    update

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -9,11 +9,15 @@ router.route("/insertproduct").post(authControllers.insertProduct);
 router.route("/scanproduct").post(authControllers.scanProduct)
 router.route("/register").post(registerAdmin)
 router.route("/login").post(loginAuth)
-router.route("/getproducts").post( authControllers.getProducts);
+router.route("/getproducts").post(authControllers.getProducts);
 router.route("/productdetail").post(authControllers.productDetails)
 router.route("/updateproduct").post(authControllers.updateProduct)
 router.route("/verifyotp/:email").post(verifyOtp)
 router.route("/soldproducts").post(authControllers.soldProducts)
 router.route("/addtocart").post(authMiddleware, authControllers.addToCart)
+router.route("/deletecart").delete(authMiddleware, authControllers.deleteCart)
+router.route("/deleteitemincart").post(authMiddleware, authControllers.deleteItem)
+router.route("/updatestock").post(authMiddleware, authControllers.updateStock)
+router.route("/getcartitems").get(authMiddleware, authControllers.getCartItems)
 
 module.exports = router
